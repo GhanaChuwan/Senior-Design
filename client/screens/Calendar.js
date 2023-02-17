e138f0fc8849d68e9b5cdd5e9f3118c2b2cdf806
ghanaC <gsc5113@psu.edu>
2023-02-17 16:00:05 -0500

    fix background header

--- client/screens/Calendar.js
+++ client/screens/Calendar.js
@@ -1,87 +1,84 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native'
-import {Agenda} from "react-native-calendars";
-import {Card,Avatar} from "react-native-paper";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Agenda } from "react-native-calendars";
+import { Card, Avatar } from "react-native-paper";
 
+export default function Calendar({ navigation, route }) {
+  useEffect(() => {
+    const parent = navigation.getParent();
+    parent?.setOptions({ title: "Calendar", headerRight: null });
+  }, [route.params]);
+  const [items, setItems] = useState({});
 
+  const timeToString = (time) => {
+    const date = new Date(time);
+    return date.toISOString().split("T")[0];
+  };
 
-export default function Calendar() {
-    const [items, setItems] = useState({});
-
-    const timeToString = (time) => {
-        const date = new Date(time);
-        return date.toISOString().split('T')[0];
-    };
-
-    const loadItems = (day) => {
-        setTimeout(() => {
-            for (let i = -15; i < 85; i++) {
-                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-                const strTime = timeToString(time);
-                if (!items[strTime]) {
-                    items[strTime] = [];
-                    const numItems = Math.floor(Math.random() * 3 + 1);
-                    for (let j = 0; j < numItems; j++) {
-                        items[strTime].push({
-                            name: 'Item for ' + strTime + ' #' + j,
-                            height: Math.max(50, Math.floor(Math.random() * 150)),
-                        });
-                    }
-                }
-            }
-            const newItems = {};
-            Object.keys(items).forEach((key) => {
-                newItems[key] = items[key];
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
             });
-            setItems(newItems);
-        }, 1000);
-    };
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
 
-    const renderItem = (item) => {
-        return (
-            <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
-                <Card>
-                    <Card.Content>
-                        <View
-                            style={{
-                                flexDirection: 'row',
-                                justifyContent: 'space-between',
-                                alignItems: 'center',
-                            }}>
-
-                            <Avatar.Text label="J" />
-                        </View>
-                    </Card.Content>
-                </Card>
-            </TouchableOpacity>
-        );
-    };
-
-
-
+  const renderItem = (item) => {
     return (
-        <View style={styles.container}>
-            <View style={styles.container}>
-                <Agenda
-                    items={items}
-                    loadItemsForMonth={loadItems}
-                    selected={'2023-02-12'}
-                    renderItem={renderItem}
-                />
+      <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
+        <Card>
+          <Card.Content>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Avatar.Text label="J" />
             </View>
-        </View>
-    )
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
 
-
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <Agenda
+          items={items}
+          loadItemsForMonth={loadItems}
+          selected={"2023-02-12"}
+          renderItem={renderItem}
+        />
+      </View>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container:{
-        flex:1
-    },
-    body:{
-        flex:1
-    }
+  container: {
+    flex: 1,
+  },
+  body: {
+    flex: 1,
+  },
 });
--- client/screens/activityScreen/ActivitySession.js
+++ client/screens/activityScreen/ActivitySession.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import CustomButton from "../../components/CustomButton/CustomButton";
+import { Card, Title } from "react-native-paper";
+import moment from "moment";
 export default function Session({ navigation, route }) {
   const { title } = route.params;
 
   useEffect(() => {
     navigation.setOptions({ headerTitle: title });
-    navigation.setOptions({ headerShown: true });
+    navigation.setOptions({
+      headerShown: true,
+      headerStyle: {
+        backgroundColor: "#1e407c",
+      },
+      headerTintColor: "#fff",
+    });
   }, [title]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Sessions</Text>
+      <View>
+        <CustomSessionsCard />
+      </View>
       <View style={{ marginTop: 80, flex: 1 }}>
         <CustomButton
           text="Add Timer"
@@ -22,6 +33,13 @@ export default function Session({ navigation, route }) {
     </View>
   );
 }
+const CustomSessionsCard = () => {
+  <Card>
+    <Card.Content>
+      <Title>hello</Title>
+    </Card.Content>
+  </Card>;
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
