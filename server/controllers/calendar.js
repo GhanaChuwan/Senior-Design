4566d83b7ba16c8c3fb3fb38e9749a05082a4b2a
gbarn360 <gbarn360@gmail.com>
2023-03-26 18:02:46 -0400

    add

--- server/controllers/calendar.js
+++ server/controllers/calendar.js
@@ -1,20 +1,25 @@
-const Calendar = require("../models/calendar");
 const User = require("../models/user");
-
+const Calendar = require("../models/calendar");
 exports.createEvents = async (req, res) => {
-  const { title, date, note } = req.body;
+  const { eventName, eventType, eventNote, eventDate } = req.body;
   const { userId } = req.user;
 
   try {
-    const user = await User.findById(userId);
+    console.log(eventName);
+    console.log(eventType);
+    console.log(eventNote);
+    console.log(eventDate);
 
-    const calendar = await Calendar.create({
-      title,
-      date,
-      note,
+    const user = await User.findById(userId);
+    console.log(user);
+    calendar = await Calendar.create({
+      eventName,
+      eventType,
+      eventNote,
+      eventDate,
       createdBy: userId,
     });
-
+    calendar.save();
     user.calendars.push(calendar._id);
     await User.findByIdAndUpdate(userId, user, {
       new: true,
@@ -29,7 +34,9 @@ exports.createEvents = async (req, res) => {
 exports.getEvents = async (req, res) => {
   const { userId } = req.user;
   try {
+    console.log("getting events");
     const calendar = await Calendar.find({ createdBy: userId });
+    console.log(calendar);
     return res.status(200).json(calendar);
   } catch (error) {
     res.status(409).json({ success: false, message: error.message });
@@ -40,6 +47,7 @@ exports.deleteEvents = async (req, res) => {
   const { userId } = req.user;
 
   try {
+    console.log("deleting events");
     const user = await User.findById(req.user.userId);
 
     user.calendars.splice(user.calendars.indexOf(calendarId), 1);
