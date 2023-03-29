bd8cff13694006d6dc6c2be8bf2f39eb068f026f
gbarn360 <gbarn360@gmail.com>
2023-03-28 20:47:59 -0400

    can now delete events

--- server/controllers/grade.js
+++ server/controllers/grade.js
@@ -77,7 +77,7 @@ exports.deleteGrade = async (req, res) => {
     console.log(subject.grades);
 
     subject.save();
-    return res.status(200).json(grades);
+    return res.status(200).json(subject.grades);
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
